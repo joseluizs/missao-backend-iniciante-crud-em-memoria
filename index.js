c6f67bb4ffdebb19fb3e55025c0c8f2997a9d17f
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-09-20 10:59:17 +0000

    Endpoint Read By Id Criado

--- index.js
+++ index.js
@@ -11,5 +11,16 @@ app.get('/personagem', function (req, res){
     res.send(lista)
 })
 
+//Endpoint Read By ID [GET]/personagem/:id
+app.get('/personagem/:id', function (req, res){
+    //acessamos o parametro de rota id
+    const id = req.params.id
+
+    //acessar o item da lista usando o ID - 1
+    const item = lista[id - 1]
+    //Enviamos o item como resposta
+    res.send(item)
+})
+
 
 app.listen(3000)
